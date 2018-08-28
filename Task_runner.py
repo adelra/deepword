3ace5a0cb1c74cce42918647a39939c354dac845
Adel Rahimi <rahimi.adel@gmail.com>
2018-08-28 11:30:59 +0430

    small fix
    + python3

--- Task_runner.py
+++ Task_runner.py
@@ -4,7 +4,7 @@ import time
 
 while True:
     print("\nStarting " + "lstm_seq2seq.py")
-    p = Popen("python " + "lstm_seq2seq.py", shell=True)
+    p = Popen("python3 " + "lstm_seq2seq.py", shell=True)
     time.sleep(14400)
     p.kill()
     print("killed")
